48ba80ef80fc5bc056407fe5888289f4d003e1fe
Mominul Hoque <55339277+mamungtg@users.noreply.github.com>
2025-12-07 00:44:38 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,9 +24,3 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = "index.html";
     });
 });
-<script>
-function toggleWAPopup() {
-    let popup = document.getElementById("whatsappPopup");
-    popup.style.display = popup.style.display === "block" ? "none" : "block";
-}
-</script>
